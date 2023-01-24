8d302cb0c8e1f7d22d5df5f1ffda24939e437320
FrankDonny <101371802+FrankDonny@users.noreply.github.com>
2023-01-24 06:08:02 +0000

    0x00-pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+"""pascal triangle module"""
+
 def pascal_triangle(n):
+    """the funciton that represents pascal triangle"""
     if n <= 0:
         return []
     triangle = [[1]]
