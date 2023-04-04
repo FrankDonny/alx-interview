2577bdc7b6f5e829c3b04ca7a84173ef0bb5471c
frankdonny <arkohfrank1@gmail.com>
2023-04-04 22:04:20 +0000

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""prime game"""
+
 
 def isWinner(x, nums):
+    """winner function"""
     def get_primes(n):
+        """fucntion to get primes"""
         sieve = [True] * (n+1)
         sieve[0] = sieve[1] = False
         for i in range(2, int(n**0.5)+1):
@@ -11,6 +15,7 @@ def isWinner(x, nums):
         return [i for i in range(n+1) if sieve[i]]
 
     def play_game(n):
+        """handles game play"""
         primes = get_primes(n)
         player = 0
         remaining = set(range(1, n+1))
